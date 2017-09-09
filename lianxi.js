90e9d399dc166acc92cb810f0c3d2d26783bc6b1
shuang <596787334@qq.com>
2017-09-09 08:31:55 +0800

    js
    JS

---
+++ lianxi.js
@@ -0,0 +1,176 @@
+/*
+* @Author: shuangshuang
+* @Date:   2017-09-04 11:03:57
+* @Last Modified by:   shuangshuang
+* @Last Modified time: 2017-09-07 17:41:27
+*/
+// var i1 = prompt ('请输入您想要的行数',5);
+// for(var i=0;i<i1;i++){
+// 			for(var m=1;m<i1-i;m++){
+// 				document.write('-');}
+//             for(var j=0;j<2*i+1;j++){
+// 			document.write('*');
+// 		   }
+// 		document.write('<br>');
+// 			}
+
+	// for(var a=0;a<i1;a++){
+ //       for(var b=0;b<a;b++){
+ //       	 document.write('-');
+ //       }
+ //       for(var c=0;c<2*(i1-a)-1;c++){
+ //       	document.write('*');
+ //       }
+ //       document.write('<br>');
+	// }
+
+// var i=1,j=1;
+// document.write(i,j);
+// for(var k=0;k<=100;){
+// 	k=i+j;
+// 	document.write(k);
+// 	document.write('&nbsp;');
+// 	i=j;
+// 	j=k;
+// }
+
+// var arr=[90,70,70,95,66];
+// var max=0;
+// for(i=0;i<arr.length;i++){
+//    if(max<arr[i]){
+//    	max=arr[i];
+//    }
+// }
+// console.log(max);
+// var arr=[90,70,70,95,66,42,6];
+// for(var i=0;i<arr.length;i++){
+// 	for(var j=i+1;j<arr.length;j++){
+//       if(arr[i]<arr[j]){
+//       	var n=arr[i];
+//       	arr[i]=arr[j];
+//       	arr[j]=n;
+//       }
+// 	}
+// }
+// document.write(arr);
+// var i=1,sum=0;
+// while(i<=10){
+//     sum+=i;
+//     i++;
+// }
+// do{
+// 	sum+=i;
+//     i++;
+// }while(i<=10)
+// alert(sum);
+// var arr=[90,70,,70,95,,66,42,6];
+// var brr=[],j=0;
+// for(var i=0;i<arr.length;i++){
+// 	if(arr[i]!=undefined){
+//        brr[j++]=arr[i];
+// 	}
+// }
+// console.log(brr);
+// // var zhangsan=[90,70,44,95,66,42,6];
+// // var lisi=[90,70,76,95,66,42,6];
+// // var wangwu=[90,70,70,95,33,42,106];
+// // var banji=[zhangsan,lisi,wangwu];
+// var banji=[[90,70,44,95,66,42,6],[90,70,76,95,66,42,6],[90,70,70,95,33,42,106]];
+// var max=0;
+// for(var a=0;a<banji.length;a++){
+//   for(var b=0;b<banji[a].length;b++){;
+//     if(max<banji[a][b]){
+//     	max=banji[a][b];
+//     }
+//   }
+// }
+// console.log(max);
+// var num=1;
+// var result=typeof num;
+// alert( typeof result);
+// var v = prompt ('请输入你想显示的行数',5);
+// function fu(i1){  //函数中的i1是形参
+// 	for(var i=0;i<i1;i++){
+// 			for(var m=1;m<i1-i;m++){
+// 				document.write('-');}
+//             for(var j=0;j<2*i+1;j++){
+// 			document.write('*');
+// 		   }
+// 		document.write('<br>');
+// 			}
+//     for(var a=0;a<i1;a++){
+//        for(var b=0;b<a;b++){
+//        	 document.write('-');
+//        }
+//        for(var c=0;c<2*(i1-a)-1;c++){
+//        	document.write('*');
+//        }
+//        document.write('<br>');
+// 	}
+	
+// }
+// fu(v);
+// var arr=[1,2,3,4,5,6,7,8];
+// fn(arr,'a','b','c');
+// function fn(arr){
+//    for(var i=1;i<arguments.length;i++){
+//    	arr[arr.length]=arguments[i];
+//    }
+//   console.log(arr);
+// }
+// var arr=[1,2,3,4,5,6,7,8];
+// fn(arr,'a','b','c');
+// function fn(arr,...value){
+//   console.log(value);
+// }
+// function filter(arr){
+// 	var arr1=[]
+// 	for(var i=0;i<arr.length;i++){
+// 		if(arr[i]%2==0){
+//          arr1[arr1.length]=arr[i];
+// 		}
+// 	}
+// 	arr=arr1;
+// 	return arr;
+// }
+// var a=[1,2,3,4,5,6,7,8,9];
+// alert(filter(a));
+// function fn(arr,str){
+//   str=str==undefined?'-':str;
+//   var string1='';
+//   for(var i=0;i<arr.length;i++){
+//   	if(i<arr.length-1){
+//       string1+=arr[i]+str;
+//   	}else{
+//       string1+=arr[i];
+//   	}
+//   }
+//   return string1;
+// }
+// var arr=[1,2,3,4,5,6,7,8,9];
+// alert(fn(a,'*'));
+  // function aa(n1,n2){
+  //   return n1+n2;
+  // }
+  // function bb(n1,n2){
+  //   return n1*n2;
+  // }
+  // function ff(n1,n2,fn){
+  // 	return fn(n1,n2);
+  // }
+  // var ee=ff(1,2,bb);
+  // alert(ee);
+
+
+  var arr=[1,2,3,4,5]
+  function map(arr,fn){
+  	var newarr=[];
+  	for(i=0;i<arr.length;i++){
+  		newarr[newarr.length]=fn(arr[i]);
+  	}
+  	return newarr;
+  }
+  var result= map (arr,function(value){
+  	 return value*2;
+  })
+  console.log(result);
